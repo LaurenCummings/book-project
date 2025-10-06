af96edb354b104cb6801a68308dfcc321ca2ad7b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-05 23:33:05 -0400

    made route to create a new book

--- backend/routes.py
+++ backend/routes.py
@@ -8,3 +8,26 @@ def get_books():
     books = Book.query.all()
     result = [book.to_json() for book in books]
     return jsonify(result)
+
+# Create a book
+@app.route("/api/books", methods=["POST"])
+def create_book():
+    try: 
+        data = request.json
+
+        title = data.get("title")
+        author = data.get("author")
+        plot = data.get("plot")
+        genre = data.get("genre")
+        img_url = data.get("imgUrl")
+
+        new_book = Book(title=title, author=author, plot=plot, genre=genre, img_url=img_url)
+
+        db.session.add(new_book)
+
+        db.session.commit()
+
+        return jsonify({"msg": "Book created successfully"}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
