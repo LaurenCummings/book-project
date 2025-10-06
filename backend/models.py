ae5bc377b9fd66fa27403af31b77e042abc104fd
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-05 22:04:55 -0400

    shortened length of genre input

--- backend/models.py
+++ backend/models.py
@@ -5,5 +5,5 @@ class Book(db.Model):
     title = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     plot = db.Column(db.Text, nullable=False)
-    genre = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
     img_url = db.Column(db.String(200), nullable=True)
