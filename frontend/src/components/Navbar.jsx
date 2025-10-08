1b37ad55f6fc4c99f6a0fc4905baf72357d2fbf8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-08 01:18:27 -0400

    added background color to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Box, Container, Flex } from "@chakra-ui/react"
 const Navbar = () => {
   return (
     <Container maxW={"900px"}>
-        <Box px={4} my={4} borderRadius={5}>
+        <Box px={4} my={4} borderRadius={5} bg={"gray.700"}>
             <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
                 <Flex alignItems={"center"} justifyContent={"center"} gap={3} display={{base:"none", sm:"flex"}}>
                     <img src="/books_large.png" alt="books image" width={50} height={50} />
