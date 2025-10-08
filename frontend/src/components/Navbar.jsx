7eac557be4d9054450add779c19cd7708dbc15ed
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-08 01:17:37 -0400

    added image and some styling to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,10 @@ const Navbar = () => {
   return (
     <Container maxW={"900px"}>
         <Box px={4} my={4} borderRadius={5}>
-            <Flex>
-                <Flex></Flex>
+            <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
+                <Flex alignItems={"center"} justifyContent={"center"} gap={3} display={{base:"none", sm:"flex"}}>
+                    <img src="/books_large.png" alt="books image" width={50} height={50} />
+                </Flex>
                 <Flex></Flex>
             </Flex>
         </Box>
