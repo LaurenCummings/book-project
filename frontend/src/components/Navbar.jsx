2478c2f1a1952c5e97c9deb07d25919ea35be20e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-08 01:12:30 -0400

    added some styling to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Box, Container, Flex } from "@chakra-ui/react"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container maxW={"900px"}>
+        <Box px={4} my={4} borderRadius={5}>
+            <Flex>
+                <Flex></Flex>
+                <Flex></Flex>
+            </Flex>
+        </Box>
+    </Container>
   )
 }
 
