98b80eb7002cf05b00a51bdd58a1cdaeb890bfaf
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-09 01:05:30 -0400

    added file for dummy data

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,44 @@
+export const BOOKS = [
+    {
+        id: 1,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },
+    {
+        id: 2,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },
+    {
+        id: 3,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },
+    {
+        id: 4,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },
+    {
+        id: 5,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },
+    {
+        id: 6,
+        title: "",
+        author: "",
+        plot: "",
+        genre: "",
+    },   
+]
