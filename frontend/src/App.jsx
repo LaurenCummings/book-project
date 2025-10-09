74b8bf428e0d44f784adeaaf8764c00d213ec1c3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-08 23:52:47 -0400

    added userGrid to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Container, Stack } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
+import UserGrid from "./components/UserGrid";
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       
       <Container maxW={"1200px"} my={4}>
-
+        <UserGrid />
 
       </Container>
     </Stack>
