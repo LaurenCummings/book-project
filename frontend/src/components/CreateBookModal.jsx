50fdadcb2639af004e724d174bbafe097fb07fed
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-09 00:38:57 -0400

    started working on new Book modal

--- frontend/src/components/CreateBookModal.jsx
+++ frontend/src/components/CreateBookModal.jsx
@@ -1,12 +1,34 @@
-import { Button } from "@chakra-ui/react"
-import { BiAddToQueue } from "react-icons/bi"
+import { Button, Flex, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
 
 const CreateBookModal = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
         <Button>
-            <BiAddToQueue />
+            <BiAddToQueue size={20} />
         </Button>
+
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Add New Book</ModalHeader>
+                <ModalCloseButton />
+
+                <ModalBody pb={6}>
+                    <Flex alignItemsd={"center"} gap={4}>
+                        <FormControl>
+                            <FormLabel></FormLabel>
+                        </FormControl>
+                    </Flex>
+                </ModalBody>
+            </ModalContent>
+
+        </Modal>
     </>
   )
 }
