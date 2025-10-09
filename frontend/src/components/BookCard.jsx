8d48dd91f957ba5103d650d4a40e5e93b7775b0c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-09 01:26:40 -0400

    started adding content to book card

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,7 +1,22 @@
+import { Box, Card, CardHeader, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 const BookCard = ({ book }) => {
   return (
-    <div>BookCard</div>
+    <Card>
+        <CardHeader>
+            <Flex gap={4}>
+                <Flex flex={"1"} gap={"4"} alignItems={"center"}>
+                    <Image src="./books.png" />
+
+                    <Box>
+                        <Heading size="sm">{book.title}</Heading>
+                        <Text>{book.author}</Text>
+                    </Box>
+                </Flex>
+                <Flex></Flex>
+            </Flex>
+        </CardHeader>
+    </Card>
   )
 }
 
