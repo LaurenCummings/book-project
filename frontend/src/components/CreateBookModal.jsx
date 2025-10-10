8bec5583416048c86f9cbb57e1571256ac7264d1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-10 01:35:25 -0400

    put create book modal inside form

--- frontend/src/components/CreateBookModal.jsx
+++ frontend/src/components/CreateBookModal.jsx
@@ -24,71 +24,73 @@ const CreateBookModal = () => {
             onClose={onClose}
         >
             <ModalOverlay />
-            <ModalContent>
-                <ModalHeader>Add New Book</ModalHeader>
-                <ModalCloseButton />
+            <form onSubmit={() => alert("book created")}>
+                <ModalContent>
+                    <ModalHeader>Add New Book</ModalHeader>
+                    <ModalCloseButton />
 
-                <ModalBody pb={6}>
-                    <Flex alignItems={"center"} gap={4}>
-                        <FormControl isRequired>
-                            <FormLabel>Title</FormLabel>
-                            <Input 
-                                placeholder="Type title here" 
-                                value={inputs.title}
-                                onChange={(e) => setInputs({...inputs, title: e.target.value})}
+                    <ModalBody pb={6}>
+                        <Flex alignItems={"center"} gap={4}>
+                            <FormControl isRequired>
+                                <FormLabel>Title</FormLabel>
+                                <Input 
+                                    placeholder="Type title here" 
+                                    value={inputs.title}
+                                    onChange={(e) => setInputs({...inputs, title: e.target.value})}
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel>Author</FormLabel>
+                                <Input 
+                                    placeholder="Type author here" 
+                                    value={inputs.author}
+                                    onChange={(e) => setInputs({...inputs, author: e.target.value})}
+                                />
+                            </FormControl>
+                        </Flex>
+                        <FormControl mt={4} isRequired>
+                            <FormLabel>Plot</FormLabel>
+                            <Textarea 
+                                placeholder="Type plot here" 
+                                value={inputs.plot}
+                                onChange={(e) => setInputs({...inputs, plot: e.target.value})}
                             />
                         </FormControl>
-                        <FormControl isRequired>
-                            <FormLabel>Author</FormLabel>
+                        <FormControl mt={4} isRequired>
+                            <FormLabel>Genre</FormLabel>
+                            <Select 
+                                placeholder="Select genre"
+                                value={inputs.genre}
+                                onChange={(e) => setInputs({...inputs, genre: e.target.value})}
+                            >
+                                <option value="mystery">Mystery</option>
+                                <option value="nonFiction">Non-Fiction</option>
+                                <option value="historicalFiction">Historical Fiction</option>
+                                <option value="fantasy">Fantasy</option>
+                                <option value="biography">Biography</option>
+                                <option value="yaFiction">YA Fiction</option>
+                                <option value="thriller">Thriller</option>
+                                <option value="dystopian">Dystopian</option>
+                            </Select>
+                        </FormControl>
+                        <FormControl mt={4}>
+                            <FormLabel>Image URL</FormLabel>
                             <Input 
-                                placeholder="Type author here" 
-                                value={inputs.author}
-                                onChange={(e) => setInputs({...inputs, author: e.target.value})}
+                                placeholder="Type image URL here" 
+                                value={inputs.imgUrl}
+                                onChange={(e) => setInputs({...inputs, imgUrl: e.target.value})}
                             />
                         </FormControl>
-                    </Flex>
-                    <FormControl mt={4} isRequired>
-                        <FormLabel>Plot</FormLabel>
-                        <Textarea 
-                            placeholder="Type plot here" 
-                            value={inputs.plot}
-                            onChange={(e) => setInputs({...inputs, plot: e.target.value})}
-                        />
-                    </FormControl>
-                    <FormControl mt={4} isRequired>
-                        <FormLabel>Genre</FormLabel>
-                        <Select 
-                            placeholder="Select genre"
-                            value={inputs.genre}
-                            onChange={(e) => setInputs({...inputs, genre: e.target.value})}
-                        >
-                            <option value="mystery">Mystery</option>
-                            <option value="nonFiction">Non-Fiction</option>
-                            <option value="historicalFiction">Historical Fiction</option>
-                            <option value="fantasy">Fantasy</option>
-                            <option value="biography">Biography</option>
-                            <option value="yaFiction">YA Fiction</option>
-                            <option value="thriller">Thriller</option>
-                            <option value="dystopian">Dystopian</option>
-                        </Select>
-                    </FormControl>
-                    <FormControl mt={4}>
-                        <FormLabel>Image URL</FormLabel>
-                        <Input 
-                            placeholder="Type image URL here" 
-                            value={inputs.imgUrl}
-                            onChange={(e) => setInputs({...inputs, imgUrl: e.target.value})}
-                        />
-                    </FormControl>
-                </ModalBody>
+                    </ModalBody>
 
-                <ModalFooter>
-                    <Button colorScheme="blue" mr={3}>
-                        Add
-                    </Button>
-                    <Button onClick={onClose}>Cancel</Button>
-                </ModalFooter>
-            </ModalContent>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3}>
+                            Add
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>                
+            </form>
 
         </Modal>
     </>
