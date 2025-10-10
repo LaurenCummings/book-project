af89858690c5d2fd8c08fd5f3268f9da5107a3c6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-10 01:32:24 -0400

    set form controls to update state on input change

--- frontend/src/components/CreateBookModal.jsx
+++ frontend/src/components/CreateBookModal.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const CreateBookModal = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [isLoading, setIsLoading] = useState(false);
-    const [inputs, setInput] = useState({
+    const [inputs, setInputs] = useState({
         title: "",
         author: "",
         plot: "",
@@ -32,20 +32,36 @@ const CreateBookModal = () => {
                     <Flex alignItems={"center"} gap={4}>
                         <FormControl isRequired>
                             <FormLabel>Title</FormLabel>
-                            <Input placeholder="Type title here" />
+                            <Input 
+                                placeholder="Type title here" 
+                                value={inputs.title}
+                                onChange={(e) => setInputs({...inputs, title: e.target.value})}
+                            />
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Author</FormLabel>
-                            <Input placeholder="Type author here" />
+                            <Input 
+                                placeholder="Type author here" 
+                                value={inputs.author}
+                                onChange={(e) => setInputs({...inputs, author: e.target.value})}
+                            />
                         </FormControl>
                     </Flex>
                     <FormControl mt={4} isRequired>
                         <FormLabel>Plot</FormLabel>
-                        <Textarea placeholder="Type plot here" />
+                        <Textarea 
+                            placeholder="Type plot here" 
+                            value={inputs.plot}
+                            onChange={(e) => setInputs({...inputs, plot: e.target.value})}
+                        />
                     </FormControl>
                     <FormControl mt={4} isRequired>
                         <FormLabel>Genre</FormLabel>
-                        <Select placeholder="Select genre">
+                        <Select 
+                            placeholder="Select genre"
+                            value={inputs.genre}
+                            onChange={(e) => setInputs({...inputs, genre: e.target.value})}
+                        >
                             <option value="mystery">Mystery</option>
                             <option value="nonFiction">Non-Fiction</option>
                             <option value="historicalFiction">Historical Fiction</option>
@@ -58,7 +74,11 @@ const CreateBookModal = () => {
                     </FormControl>
                     <FormControl mt={4}>
                         <FormLabel>Image URL</FormLabel>
-                        <Input placeholder="Type image URL here" />
+                        <Input 
+                            placeholder="Type image URL here" 
+                            value={inputs.imgUrl}
+                            onChange={(e) => setInputs({...inputs, imgUrl: e.target.value})}
+                        />
                     </FormControl>
                 </ModalBody>
 
